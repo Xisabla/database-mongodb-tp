d25fc0751781f82e94ac657ec91c961a76c5ac67
Gautier Miquet <xisabla.dev@gmail.com>
2021-09-24 09:58:56 +0200

    add index for geo near finding

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, GEO2D
 from pymongo.errors import PyMongoError
 
 load_dotenv()
@@ -16,3 +16,8 @@ except PyMongoError as e:
 
 # TODO: Change for production (main branch)
 db = client.gautier
+
+db.lille.create_index([("geometry", GEO2D)])
+db.lyon.create_index([('geometry', GEO2D)])
+db.paris.create_index([('geometry', GEO2D)])
+db.rennes.create_index([('geometry', GEO2D)])
